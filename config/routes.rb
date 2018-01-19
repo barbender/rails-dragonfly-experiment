833f873b82d3131af7d0bf8fd757f5ec35dced3c
Michael Smus <michaels@evidentpoint.com>
2018-01-18 17:22:47 -0800

    Fixed routing by pluralizing resource definition

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resource :photos, only: [:index, :new, :create]
+  # restrict photos route to the following actions
+  # resources :photos, only: [:index, :new, :create]
+  # no restrictions
+  resources :photos
 
+  # no path 'root' of the app
   root to: "photos#index"
 end
 
